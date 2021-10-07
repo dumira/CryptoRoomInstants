6c90340fdd01af720fe3c6b2f2b925f80703e149
Nisal Chandrasekaram <cmnisal@gmail.com>
2021-10-07 10:35:44 +0530

    Bot Update

--- reader.py
+++ reader.py
@@ -1,9 +1,17 @@
 import feedparser
 import webbrowser
 from html2image import Html2Image
+import telebot
+from dotenv import load_dotenv
+load_dotenv()
+import os
+token = os.environ.get("token")
+
+bot = telebot.TeleBot(token, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
 hti = Html2Image()
 hti.output_path = 'img'
-
+chat_id = -469613389
 feed = feedparser.parse("https://cointelegraph.com/rss")
 
 # feed_title = feed['feed']['title']  # NOT VALID
@@ -17,11 +25,14 @@ for entry in feed.entries:
     slug =entry.guid.rsplit('/', 1)[1]
     instant_url = 'https://ceyloncash.com/instants/?text='+title+'&imgurl='+mediaContent
     file_name = slug+'.png'
-
-    print(title+"<br>"+hashtag)
+    description = title+"<br>"+hashtag
+    print(description)
     print(mediaContent)
     print(file_name)
     print(hashtag)
     print("################")
+    photo = open('img\\'+file_name, 'rb')
+    bot.send_photo(chat_id, photo)
+    # bot.send_message(chat_id,description)
 
     hti.screenshot(url=instant_url, save_as=file_name,size=(1080, 1080))
