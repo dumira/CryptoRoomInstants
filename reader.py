50b0d16d030cecbcacf4cb00cb079a886547c92e
dumira <dumira1990@gmail.com>
2021-10-14 15:02:35 +0530

    loop added

--- reader.py
+++ reader.py
@@ -1,3 +1,4 @@
+from apscheduler.schedulers.blocking import BlockingScheduler as scheduler
 from base64 import encodestring
 import feedparser
 import webbrowser
@@ -12,79 +13,92 @@ import glob
 from dotenv import load_dotenv
 load_dotenv()
 
-# clear image folder
-files = glob.glob('img/*')
-for f in files:
-    os.remove(f)
 
-token = os.environ.get("token")
+def timerFunction():
+    # clear image folder
+    files = glob.glob('img/*')
+    for f in files:
+        os.remove(f)
 
-# You can set parse_mode by default. HTML or MARKDOWN
-bot = telebot.TeleBot(token, parse_mode="HTML")
+    token = os.environ.get("token")
 
-hti = Html2Image()
-hti.output_path = 'img'
-chat_id = -469613389
-feed = feedparser.parse("https://cointelegraph.com/rss")
+    # You can set parse_mode by default. HTML or MARKDOWN
+    bot = telebot.TeleBot(token, parse_mode="HTML")
 
-# load json
-try:
-    lastItem = pd.read_json('json/lastItem.json')
-    lastItemId = lastItem['id'].iloc[0]
-    firstRun = 0
-except:
-    lastItemId = None
-    firstRun = 1
+    hti = Html2Image()
+    hti.output_path = 'img'
+    chat_id = -469613389
+    feed = feedparser.parse("https://cointelegraph.com/rss")
 
-item_df = pd.DataFrame(columns=['id', 'pubDate', 'description', 'image'])
-
-for entry in feed.entries:
-    if 'media_content' in entry:
-        mediaContent = entry.media_content[0]['url']
-    title = entry.title
-    pubDate = entry.published
-    id = entry.id
-
-    # skip if exsisting post
-    if(lastItemId==id):
-        # if no new post get previous df
-        if(item_df.shape[0]==0):
-            item_df = lastItem.copy()
-        break
-
+    # load json
     try:
-        hashtag = "#"+entry.category.replace(" ","")
+        lastItem = pd.read_json('json/lastItem.json')
+        lastItemId = lastItem['id'].iloc[0]
+        firstRun = 0
     except:
-        hashtag = ""
+        lastItemId = None
+        firstRun = 1
 
-    slug =entry.guid.rsplit('/', 1)[1]
-    file_name = slug+'.png'
-    description = title+" "+hashtag
+    item_df = pd.DataFrame(columns=['id', 'pubDate', 'description', 'image'])
 
-    print(file_name)
-    print(hashtag)
-    print("################")
+    for entry in feed.entries:
+        if 'media_content' in entry:
+            mediaContent = entry.media_content[0]['url']
+        title = entry.title
+        pubDate = entry.published
+        id = entry.id
 
-    row = {'id': id, 'pubDate': pubDate, 'description': description, 'image': mediaContent}
-    item_df = item_df.append(row, ignore_index=True)
+        # skip if exsisting post
+        if(lastItemId==id):
+            # if no new post get previous df
+            if(item_df.shape[0]==0):
+                item_df = lastItem.copy()
+            break
 
-    instant_url = 'https://ceyloncash.com/instants/?text='+urllib.parse.quote_plus(description)+'&imgurl='+mediaContent
-    hti.screenshot(url=(instant_url), save_as=file_name,size=(1080, 1080))
+        try:
+            hashtag = "#"+entry.category.replace(" ","")
+        except:
+            hashtag = ""
 
-    photo = open('img\\'+file_name, 'rb')
-    markup = types.InlineKeyboardMarkup(row_width=2)
-    itembtn1 = types.InlineKeyboardButton('Broadcast',callback_data='/send')
-    itembtn2 = types.InlineKeyboardButton('Ignore',callback_data='/ignore')
-    markup.add(itembtn1, itembtn2)
-    bot.send_photo(chat_id,photo, caption=description+" | "+mediaContent+" | "+id, reply_markup=markup)
+        slug =entry.guid.rsplit('/', 1)[1]
+        file_name = slug+'.png'
+        description = title+" "+hashtag
 
-    # If first run take only first item
-    if(firstRun==1):
-        break
+        print(file_name)
+        print(hashtag)
+        print("################")
 
-    # bot.send_photo(chat_id, photo)
-    # bot.send_message(chat_id,description)
+        row = {'id': id, 'pubDate': pubDate, 'description': description, 'image': mediaContent}
+        item_df = item_df.append(row, ignore_index=True)
 
-item_df['pubDate'] = pd.to_datetime(item_df['pubDate'])
-item_df = item_df.head(1)
-item_df.to_json('json/lastItem.json')
+        instant_url = 'https://ceyloncash.com/instants/?text='+urllib.parse.quote_plus(description)+'&imgurl='+mediaContent
+        hti.screenshot(url=(instant_url), save_as=file_name,size=(1080, 1080))
+
+        photo = open('img\\'+file_name, 'rb')
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        itembtn1 = types.InlineKeyboardButton('Broadcast', callback_data='/send')
+        itembtn2 = types.InlineKeyboardButton('Ignore', callback_data='/ignore')
+        itembtn3 = types.InlineKeyboardButton('Read More..', url=id)
+
+        markup.add(itembtn1, itembtn2, itembtn3)
+        bot.send_photo(chat_id,photo, caption=description+" | "+mediaContent, reply_markup=markup)
+
+        # If first run take only first item
+        if(firstRun==1):
+            break
+
+        # bot.send_photo(chat_id, photo)
+        # bot.send_message(chat_id,description)
+
+    item_df['pubDate'] = pd.to_datetime(item_df['pubDate'])
+    item_df = item_df.head(1)
+    item_df.to_json('json/lastItem.json')
+
+    print("$$##$$")
+
+# Execute your code before starting the scheduler
+print('Starting scheduler, ctrl-c to exit!')
+
+sch = scheduler()
+sch.add_job(timerFunction, 'interval', seconds=60)
+sch.start()
