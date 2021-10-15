0eeb4e4de5ef59396d2208f92200fa3ae2e49e7f
dumira <dumira1990@gmail.com>
2021-10-15 08:59:54 +0530

    Update time issue

--- reader.py
+++ reader.py
@@ -7,11 +7,16 @@ import telebot
 from telebot import types
 import urllib.parse
 import pandas as pd
+from datetime import datetime
 
 import os
 import glob
 from dotenv import load_dotenv
 load_dotenv()
+
+pd.set_option('display.max_column', None)
+pd.set_option('display.max_row', None)
+pd.set_option('display.float_format', lambda x: '%.2f' % x)
 
 
 def timerFunction():
@@ -34,10 +39,13 @@ def timerFunction():
     try:
         lastItem = pd.read_json('json/lastItem.json')
         lastItemId = lastItem['id'].iloc[0]
+        pubTime = lastItem['pubDate'].iloc[0]
         firstRun = 0
     except:
         lastItemId = None
+        pubTime = "Thu, 14 Oct 2021 01:01:01 +0100"
         firstRun = 1
+
 
     item_df = pd.DataFrame(columns=['id', 'pubDate', 'description', 'image'])
 
@@ -48,8 +56,9 @@ def timerFunction():
         pubDate = entry.published
         id = entry.id
 
-        # skip if exsisting post
-        if(lastItemId==id):
+
+        # skip if exsisting post or previous post
+        if((lastItemId==id) | (pd.to_datetime(pubDate)<=pd.to_datetime(pubTime))):
             # if no new post get previous df
             if(item_df.shape[0]==0):
                 item_df = lastItem.copy()
@@ -74,23 +83,19 @@ def timerFunction():
         instant_url = 'https://ceyloncash.com/instants/?text='+urllib.parse.quote_plus(description)+'&imgurl='+mediaContent
         hti.screenshot(url=(instant_url), save_as=file_name,size=(1080, 1080))
 
-        photo = open('img\\'+file_name, 'rb')
-        markup = types.InlineKeyboardMarkup(row_width=2)
-        itembtn1 = types.InlineKeyboardButton('Broadcast', callback_data='/send')
-        itembtn2 = types.InlineKeyboardButton('Ignore', callback_data='/ignore')
-        itembtn3 = types.InlineKeyboardButton('Read More..', url=id)
-
-        markup.add(itembtn1, itembtn2, itembtn3)
-        bot.send_photo(chat_id,photo, caption=description+" | "+mediaContent, reply_markup=markup)
+        # photo = open('img\\'+file_name, 'rb')
+        # markup = types.InlineKeyboardMarkup(row_width=2)
+        # itembtn1 = types.InlineKeyboardButton('Broadcast', callback_data='/send')
+        # itembtn2 = types.InlineKeyboardButton('Ignore', callback_data='/ignore')
+        # itembtn3 = types.InlineKeyboardButton('Read More..', url=id)
+        #
+        # markup.add(itembtn1, itembtn2, itembtn3)
+        # bot.send_photo(chat_id,photo, caption=description+" | "+mediaContent, reply_markup=markup)
 
         # If first run take only first item
         if(firstRun==1):
             break
 
-        # bot.send_photo(chat_id, photo)
-        # bot.send_message(chat_id,description)
-
-    item_df['pubDate'] = pd.to_datetime(item_df['pubDate'])
     item_df = item_df.head(1)
     item_df.to_json('json/lastItem.json')
 
